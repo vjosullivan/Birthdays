aa9d6d28283b7740eb90c1458767216853ef1caf
vjosullivan <vjosullivan@hotmail.com>
2018-07-21 14:04:27 +0100

    Initial Commit

---
+++ Birthdays/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Birthdays
+//
+//  Created by Vincent O'Sullivan on 21/07/2018.
+//  Copyright © 2018 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
