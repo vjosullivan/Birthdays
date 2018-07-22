abcee04248293bec46d43a4342f326c04e5caa26
vjosullivan <vjosullivan@hotmail.com>
2018-07-22 20:27:33 +0100

    Add sort from month

--- Birthdays/BirthdayModel/Birthdays.swift
+++ Birthdays/BirthdayModel/Birthdays.swift
@@ -30,6 +30,20 @@ struct Birthdays {
         return birthdays.sorted()
     }
 
+    /// Returns an array of birthdays sorted from the beginning of the given month and wrapping around the
+    /// year end, ending before arriving back at the given month.  Note that the year
+    /// of birth is not part of the birthday sort order, only the year and day.  The sort order of
+    /// multiple same birthdays is not determined.
+    ///
+    /// - Parameter month: The birth month from which to start the sort.
+    /// - Returns: A sorted array of birthdays.
+    ///
+    func sorted(month: Int) -> [Birthday] {
+        let earlierBirthdays = birthdays.filter({ $0.month <  month }).sorted()
+        let laterBirthdays   = birthdays.filter({ $0.month >= month }).sorted()
+        return laterBirthdays + earlierBirthdays
+    }
+
     subscript(_ index: Int) -> Birthday {
         get {
             return birthdays[index]
--- BirthdaysTests/BirthdayModel/BirthdaysTests.swift
+++ BirthdaysTests/BirthdayModel/BirthdaysTests.swift
@@ -46,4 +46,19 @@ class BirthdaysTests: XCTestCase {
     func testSortabilityWhenEmpty() {
         XCTAssertEqual(0, Birthdays().sorted().count)
     }
+
+    /// Can sort from (for instance: July), wrap around the new year, and end in (for instance: June).
+    func testSortabilityByMonth() {
+        let b06_30 = Birthday(name: "June Ender",   year: 1961, month:  6, day: 30)
+        let b06_04 = Birthday(name: "June Early",   year: 1962, month:  6, day:  4)
+        let b04_13 = Birthday(name: "April Shower", year: 1963, month:  4, day: 13)
+        let b11_04 = Birthday(name: "Dr November",  year: 1964, month: 11, day:  4)
+        let b07_01 = Birthday(name: "Julie Just",   year: 1965, month:  7, day:  4)
+        let birthdays = Birthdays([b06_04, b11_04, b04_13, b06_30]).sorted(month: 7)
+        XCTAssertEqual(b04_13, birthdays[2])
+        XCTAssertEqual(b06_04, birthdays[3])
+        XCTAssertEqual(b06_30, birthdays[4])
+        XCTAssertEqual(b07_01, birthdays[0])
+        XCTAssertEqual(b11_04, birthdays[1])
+    }
 }
